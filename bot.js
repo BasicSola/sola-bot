a4f3f0d5e72e3218c994f825614fdd5d162e755d
BasicSola <46502356+BasicSola@users.noreply.github.com>
2019-01-08 23:39:49 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,52 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+bot.on('ready', () => {
+    console.log("Turning On Bot")
+    bot.user.setActivity("Sola's moans", {type: 2});
+});
+
+bot.on('guildMemberAdd', member => {
+    let channel = member.guild.channels.find('name', 'general');
+    let memberavatar = member.user.avatarURL
+        if (!channel) return;
+        let embed = new Discord.RichEmbed()
+        .setColor('0x1BF235')
+        .setThumbnail(memberavatar)
+        .addField(':bust_in_silhouette: | Name : ', `${member}`)
+        .addField(':microphone2: | Welcome!', `Welcome to the server, ${member} please check #read-me and #rules.`)
+        .addField(':family_mwgb: | You are the member', `${member.guild.memberCount}`)
+        .setFooter(`${member.guild.name}`)
+        .setTimestamp()
+
+        channel.sendEmbed(embed);
+});
+
+bot.on('guildMemberAdd', member => {
+
+    console.log(`${member}`, "has joined" + `${member.guild.name}`)
+
+});
+
+bot.on('guildMemberRemove', member => {
+    let channel = member.guild.channels.find('name', 'general');
+    let memberavatar = member.user.avatarURL
+        if (!channel) return;
+        let embed = new Discord.RichEmbed()
+        .setColor('0xFF0D00')
+        .setThumbnail(memberavatar)
+        .addField(':bust_in_silhouette: | Name:', `${member}`)
+        .addField(':microphone2: | Has left the server', 'We will miss you!')
+        .addField(':family_mwgb: | The server now has', `${member.guild.memberCount}` + " members")
+        .setFooter(`${member.guild.name}`)
+        .setTimestamp()
+
+        channel.sendEmbed(embed);
+});
+
+bot.on('guildMemberRemove', member => {
+    console.log(`${member}` + "has left" + `${member.guild.name}` + "Sending leave message now")
+    console.log("Leave Message Sent")
+});
+
+const token = process.env.TOKEN;
